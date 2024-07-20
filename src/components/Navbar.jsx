1a6e225c1372e17759a6a87472ae788bae648d6f
mmarya045 <marya.madi@epitech.eu>
2024-07-20 22:34:19 +0400

    second part of the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
             <div>
                 <div>
                     <a href="/"><img src={logo} alt='' className="inline-block items-center px-[100px] mt-1"/></a>
-                <div className="w-[1512px] h-[142px] px-[150px] justify-start items-center gap-24 inline-flex">
+                <div className="w-[1512px] h-[142px] px-[190px] justify-start items-center gap-24 inline-flex">
                     <div className="w-[95px] h-[24.94px] relative"></div>
                     <div className="h-[51px] px-[60px] py-3 bg-stone-100 rounded-[100px] justify-center items-center gap-[30px] flex">
                         <div className="p-3 justify-center items-center gap-3 flex">
@@ -20,8 +20,16 @@ const Navbar = () => {
                             <div className="text-dark font-bold">Blog</div>
                         </div>
                     </div>
+                    <div className="w-[50px] relative">
+                        <div className="w-[183px] h-[51px] px-[20px] py-5 mx-40 bg-white rounded-[100px] justify-center items-center border border-darkgrey gap-3 inline-flex">
+                            <div className="text-dark font-bold">Espace créateur</div>
+                        </div>
+                        <div className="w-[197px] h-[51px] px-[20px] py-5 left-[365px] top-0 absolute bg-violet rounded-[100px] justify-center items-center  gap-3 inline-flex">
+                            <div className="text-white font-bold">Blog</div>
+                        </div>
+                    </div>
                 </div>
-            </div>
+                </div>
             </div>
         </nav>
     )
