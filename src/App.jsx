24c914d7a7f399ea46b748e77e939387085c4824
mmarya045 <marya.madi@epitech.eu>
2024-07-20 12:02:19 +0400

    v0: video player

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import reactLogo from './assets/react.svg'
+import VideoPlayer from './component/videoplayer'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <VideoPlayer/>
+      </div>
+    </>
+  )
+}
+
+export default App
