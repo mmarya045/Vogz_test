8816e9e76f6e271181c1dd1e50ba5e7c88ca0801
mmarya045 <marya.madi@epitech.eu>
2024-07-24 00:19:10 +0400

    Implement HLS

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env": {
+      REACT_APP_TOKEN_KEY: "",
+      REACT_APP_SECRET_KEY: "",
+      ENV_KEY: ""
+    }
+  }
 })
